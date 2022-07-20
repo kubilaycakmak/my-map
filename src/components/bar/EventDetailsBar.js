3ed18794bc2dc1c695642685683ed97c655c2c26
Kubilay Cakmak <kubilayckmk@gmail.com>
2022-07-20 00:17:05 -0700

    mobile responsive v1.0

--- src/components/bar/EventDetailsBar.js
+++ src/components/bar/EventDetailsBar.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './eventdetailsbar.module.scss'
 
 const EventDetailsBar = ({step}) => {
 
+  const [width, setWidth ] = useState();
+
+
+  useEffect(() => {
+    const { innerWidth: width, innerHeight: height } = window;
+    setWidth(width)
+  }, [width])
+  
+
   return (
     <div className={styles.bar}>
-        <NavLink to={"/event"}> Back to Events</NavLink>
+        { width >= 768 ? <NavLink to={"/event"}> Back to Events</NavLink> : <NavLink to={"/event"}> Back</NavLink> }
 
         <div className={styles.steps}>
-            <div className={step == 0 ? styles.active : styles.disable}>Event Details</div>
-            <div className={step == 1 ? styles.active : styles.disable}>Distribution Options</div>
-            <div className={step == 2 ? styles.active : styles.disable}>Rewards</div>
-            <div className={step == 3 ? styles.active : styles.disable}>Review & Publish</div>
+          {
+            width >= 768 ?
+
+            <>
+              <div className={step == 0 ? styles.active : styles.disable}>Event Details</div>
+              <div className={step == 1 ? styles.active : styles.disable}>Distribution Options</div>
+              <div className={step == 2 ? styles.active : styles.disable}>Rewards</div>
+              <div className={step == 3 ? styles.active : styles.disable}>Review & Publish</div>
+            </>
+            :
+            <>
+              <div className={step == 0 || step > 0 ? styles.activeMobile : styles.disableMobile}>{step == 0 ? 1 : "👍🏼"}</div>
+              <div className={step > 0 ? styles.activeMobile : styles.disableMobile}>{step > 1 ? "👍🏼" : 2}</div>
+              <div className={step > 1 ? styles.activeMobile : styles.disableMobile}>{step > 2 ? "👍🏼" : 3}</div>
+              <div className={step > 2 ? styles.activeMobile : styles.disableMobile}>{step > 3 ? "👍🏼" : 4}</div>
+            </>
+          }
+            
         </div>
     </div>
   )
